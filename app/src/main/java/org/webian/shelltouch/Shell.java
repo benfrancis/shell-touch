c09b8a11c8c08cba7f4f8c6b98f90db94296e3ce
Ben Francis <ben@tola.me.uk>
2019-01-04 15:40:45 +0000

    Add window button and cleanup

--- app/src/main/java/org/webian/shelltouch/Shell.java
+++ app/src/main/java/org/webian/shelltouch/Shell.java
@@ -5,6 +5,7 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 import java.util.ArrayList;
 
@@ -12,21 +13,24 @@ import java.util.ArrayList;
 /**
  * Webian Shell Touch.
  */
-public class HomeActivity extends Activity {
+public class Shell extends Activity {
 
     private ShellDatabase database;
-    private View mContentView;
+    private View shellContent;
     private HomeScreenWindow homeScreenWindow;
     private TaskManager taskManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_shell);
+        shellContent = findViewById(R.id.shell_content);
+
+        // Start database
         database = new ShellDatabase(getApplicationContext());
         ArrayList apps = database.getApps();
         System.out.println("Apps: " + apps);
-        mContentView = findViewById(R.id.home_content);
+
 
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
@@ -56,6 +60,12 @@ public class HomeActivity extends Activity {
         transaction.hide(taskManager);
         transaction.show(homeScreenWindow);
         transaction.commit();
+
+        // Hide add window button and show windows button
+        ImageButton windowsButton = (ImageButton) findViewById(R.id.windows_button);
+        ImageButton addWindowButton = (ImageButton) findViewById(R.id.add_window_button);
+        windowsButton.setVisibility(View.VISIBLE);
+        addWindowButton.setVisibility(View.GONE);
     }
 
     /**
@@ -78,6 +88,12 @@ public class HomeActivity extends Activity {
         transaction.hide(homeScreenWindow);
         transaction.show(taskManager);
         transaction.commit();
+
+        // Hide windows button and show add window button
+        ImageButton windowsButton = (ImageButton) findViewById(R.id.windows_button);
+        ImageButton addWindowButton = (ImageButton) findViewById(R.id.add_window_button);
+        windowsButton.setVisibility(View.GONE);
+        addWindowButton.setVisibility(View.VISIBLE);
     }
 
     /**
@@ -93,7 +109,7 @@ public class HomeActivity extends Activity {
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) {
-            mContentView.setSystemUiVisibility(
+            shellContent.setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
